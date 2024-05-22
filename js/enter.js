3538b30a0b3eb41badb322f64535e253fa532124
unel <enee.prog@gmail.com>
2024-05-22 23:30:50 +0200

    add responsive for index.html

--- js/enter.js
+++ js/enter.js
@@ -20,3 +20,25 @@ myElements.forEach(myElement => {
 	observerEnter.observe(myElement);
 	observerLeave.observe(myElement);
 });
+
+const images = document.querySelectorAll('.enterAnimationImage');
+images.forEach(myElement => {
+	let isAnimated = false;
+
+	const observerEnter = new IntersectionObserver(entries => {
+		if (entries[0].isIntersecting && !isAnimated) {
+			myElement.style.animation = 'enterAnimationImageAnim 0.5s linear forwards';
+			isAnimated = true;
+		}
+	});
+
+	const observerLeave = new IntersectionObserver(entries => {
+		if (!entries[0].isIntersecting && isAnimated) {
+			myElement.style.animation = 'none';
+			isAnimated = false;
+		}
+	});
+
+	observerEnter.observe(myElement);
+	observerLeave.observe(myElement);
+});
